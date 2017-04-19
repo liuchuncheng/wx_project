66ae4a510ddcc6d93d9f358c5f5287a6e52fe406
zhangyi <q1289303698@qq.com>
2017-04-19 10:57:37 +0800

    edit

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -12,7 +12,7 @@ return array(
 	"DB_HOST"=>"localhost",
 	"DB_NAME"=>"wxproject",
 	"DB_USER"=>"root",
-	"DB_PWD"=>"",
+	"DB_PWD"=>"root",
 	// "DB_PWD"=>"root",
 	"DB_PORT"=>"3306",
 	"DB_PREFIX"=>"pt_",
