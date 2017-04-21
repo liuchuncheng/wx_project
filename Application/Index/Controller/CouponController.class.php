c0b07a0937208f7f31dc0bb4170da5cbf7dc2e68
jx <2238966232@qq.com>
2017-04-21 14:21:59 +0800

    ddd

--- Application/Index/Controller/CouponController.class.php
+++ Application/Index/Controller/CouponController.class.php
@@ -8,7 +8,7 @@ class CouponController extends Controller
 	{
         $order=M()->query("SELECT * FROM pt_order where id=$id");
         $gid=$order[0]['gid'];
-        $order_num=$order[0]['order_num'];
+        $order_num=$order[0]['thridsqo'];
         $data=M()->query("SELECT a.*,b.name as shop_name FROM pt_goods a left join pt_shop b on a.sid=b.id where a.id = $gid");
 		$num=M()->query("SELECT count(*) as num from pt_order where gid=$gid and status=2");
 		$this->assign('data',$data);
